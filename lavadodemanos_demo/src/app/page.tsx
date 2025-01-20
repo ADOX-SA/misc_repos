7ea146f01d0ffc307e2e1cd2f3b9f20bd96b213c
alberto sanchez <52971416+albertoleandrosanchez@users.noreply.github.com>
2025-01-20 09:35:38 -0300

    lavado de manos subido

---
+++ lavadodemanos_demo/src/app/page.tsx
@@ -0,0 +1,99 @@
+"use client";
+import "@tensorflow/tfjs-backend-webgl"; // set backend to webgl
+
+import React, { useRef, useState } from "react";
+import Webcam from "react-webcam";
+import * as tf from "@tensorflow/tfjs";
+import { useEffect } from "react";
+import ButtonHandler from "@/components/btn-handler";
+import Loader from "@/components/loader";
+import { detect, detectVideo } from "../utils/detect";
+import "../style/App.css";
+
+// import "@tensorflow/tfjs-backend-webgl";
+
+export default function Home() {
+  const [loading, setLoading] = useState({ loading: true, progress: 0 }); // loading state
+  const [model, setModel] = useState({
+    net: null,
+    inputShape: [1, 0, 0, 3],
+  });
+  const imageRef = useRef(null);
+  const cameraRef = useRef(null);
+  const videoRef = useRef(null);
+  const canvasRef = useRef(null);
+
+  const modelName = "hands_model";
+  useEffect(() => {
+    tf.ready().then(async () => {
+      const yolov8 = await tf.loadGraphModel(
+        `${window.location.href}/${modelName}/model.json`,
+        {
+          onProgress: (fractions) => {
+            setLoading({ loading: true, progress: fractions }); // set loading fractions
+          },
+        }
+      ); // load model
+
+      // warming up model
+      const dummyInput = tf.ones(yolov8.inputs[0].shape || [1, 224, 224, 3]);
+      const warmupResults = yolov8.execute(dummyInput);
+
+      setLoading({ loading: false, progress: 1 });
+      setModel({
+        net: yolov8,
+        inputShape: yolov8.inputs[0].shape,
+      }); // set model & input shape
+
+      tf.dispose([warmupResults, dummyInput]); // cleanup memory
+    });
+  }, []);
+  return (
+    <div className="grid place-items-center">
+      <div className="App">
+        {loading.loading && (
+          <Loader>
+            Loading model... {(loading.progress * 100).toFixed(2)}%
+          </Loader>
+        )}
+        <div className="header">
+          <h1>📷 YOLOv8 Live Detection App</h1>
+          <p>
+            YOLOv8 live detection application on browser powered by{" "}
+            <code>tensorflow.js</code>
+          </p>
+          <p>
+            Serving : <code className="code">{modelName}</code>
+          </p>
+        </div>
+
+        <div className="content">
+          <img
+            src="#"
+            ref={imageRef}
+            onLoad={() => detect(imageRef.current, model, canvasRef.current)}
+          />
+          <video
+            autoPlay
+            muted
+            ref={cameraRef}
+            onPlay={() =>
+              detectVideo(cameraRef.current, model, canvasRef.current)
+            }
+          />
+          <canvas
+            width={model.inputShape[1]}
+            height={model.inputShape[2]}
+            ref={canvasRef}
+          />
+        </div>
+
+        <ButtonHandler
+          imageRef={imageRef}
+          cameraRef={cameraRef}
+          videoRef={videoRef}
+        />
+      </div>
+    </div>
+  );
+}
---
+++ lavadodemanos_demo/src/tfjs/setup.ts
@@ -0,0 +1,80 @@
+import * as tf from "@tensorflow/tfjs";
+import { RefObject } from "react";
+import Webcam from "react-webcam";
+
+async function loadModel() {
+  return tf.loadGraphModel("/hands_model/model.json");
+}
+
+const hands_model_setup = async () => {
+  const model = await loadModel();
+  return model;
+};
+
+export const detect = async (
+  model: tf.GraphModel,
+  webcam: RefObject<Webcam | null>,
+  canvas: RefObject<HTMLCanvasElement | null>
+) => {
+  if (
+    webcam != null &&
+    canvas != null &&
+    typeof webcam.current != "undefined" &&
+    webcam.current != null &&
+    webcam.current.video &&
+    webcam.current.video.readyState === 4
+  ) {
+    // Get Video Properties
+    const video = webcam.current.video;
+    const canva = canvas.current;
+    if (canva == null) {
+      console.log("canvas is null");
+      return;
+    }
+    const videoWidth = video.videoWidth;
+    const videoHeight = video.videoHeight;
+    // Set video width
+    video.width = videoWidth;
+    video.height = videoHeight;
+    // Set canvas width
+    canva.width = videoWidth;
+    canva.height = videoHeight;
+    const tensor = tf.browser.fromPixels(video);
+    const resizedTensor = tf.image
+      .resizeBilinear(tensor, [videoWidth, videoHeight])
+      .div(255);
+    const tensorWithBatchSize = resizedTensor.expandDims(0);
+    // Make Detections
+    const output = await model.executeAsync(tensorWithBatchSize);
+    const boxes = output;
+    // const scores = output[1].arraySync();
+    // const classes = output[2].dataSync();
+
+    console.log(boxes);
+    // Draw mesh
+    // const ctx = canva.getContext("2d");
+    // if (ctx == null) {
+    //   console.log("context is null");
+    //   return;
+    // }
+
+    //obtener la prediccion paar saber que paso es el que se esta dibujando
+    // let predictionValues;
+    // if (Array.isArray(predictions)) {
+    //   predictionValues = await Promise.all(
+    //     predictions.map((pred) => pred.)
+    //   );
+    // } else if (predictions instanceof tf.Tensor) {
+    //   predictionValues = await predictions.data();
+    // } else {
+    //   predictionValues = Object.values(predictions).map(
+    //     async (pred) => await pred.data()
+    //   );
+    //   predictionValues = await Promise.all(predictionValues);
+    // }
+
+    // console.log(predictionValues);
+  }
+};
+
+export default hands_model_setup;
