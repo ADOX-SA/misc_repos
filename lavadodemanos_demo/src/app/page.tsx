8f64725a92884bcda34680e8e7d8f159fbd4c003
Ivorra-1996 <Ivorra-1996>
2025-02-26 10:09:32 -0300

    Copia de beto

--- lavadodemanos_demo/src/app/page.tsx
+++ lavadodemanos_demo/src/app/page.tsx
@@ -132,3 +132,138 @@ export default function Home() {
     </div>
   );
 }
+
+
+// TODO: Cosas de beto page.tsx
+// "use client";
+// import "@tensorflow/tfjs-backend-webgl"; // set backend to webgl
+
+// import React, { useRef, useState } from "react";
+// import Webcam from "react-webcam";
+// import * as tf from "@tensorflow/tfjs";
+// import { useEffect } from "react";
+// import ButtonHandler from "@/components/btn-handler";
+// import Loader from "@/components/loader";
+// import { detect, detectAllClasses, detectVideo } from "../utils/detect";
+// import style from "../style/App.module.css";
+// import "../style/App.css";
+
+// // import "@tensorflow/tfjs-backend-webgl";
+
+// export default function Home() {
+//   const [predicciones, setPredicciones] = useState([
+//     { clase: "Cargando...", score: 0 },
+//   ]); // prediction state
+//   const [loading, setLoading] = useState({ loading: true, progress: 0 }); // loading state
+//   const [model, setModel] = useState({
+//     net: null,
+//     inputShape: [1, 0, 0, 3],
+//   });
+//   const imageRef = useRef(null);
+//   const cameraRef = useRef(null);
+//   const videoRef = useRef(null);
+//   const canvasRef = useRef(null);
+
+//   const modelName = "hands_model";
+//   useEffect(() => {
+//     tf.ready().then(async () => {
+//       const yolov8 = await tf.loadGraphModel(
+//         `${window.location.href}/${modelName}/model.json`,
+//         {
+//           onProgress: (fractions) => {
+//             setLoading({ loading: true, progress: fractions }); // set loading fractions
+//           },
+//         }
+//       ); // load model
+
+//       // warming up model
+//       const dummyInput = tf.ones(yolov8.inputs[0].shape || [1, 224, 224, 3]);
+//       const warmupResults = yolov8.execute(dummyInput);
+//       if (!yolov8) return;
+//       setLoading({ loading: false, progress: 1 });
+//       setModel({
+//         net: yolov8,
+//         inputShape: yolov8.inputs[0].shape,
+//       }); // set model & input shape
+
+//       tf.dispose([warmupResults, dummyInput]); // cleanup memory
+//     });
+//   }, []);
+//   return (
+//     <div className={style.centeredGrid}>
+//       <div className={style.app}>
+//         {loading.loading && (
+//           <Loader
+//             text="Cargando modelo..."
+//             progress={(loading.progress * 100).toFixed(2)}
+//           />
+//         )}
+//         <div className={style.header}>
+//           <img src="/LogoAdox.png" alt="Logo de ADOX" />
+
+//           <p>¡Bienvenido a ADOX HandWash AI! 👏</p>
+//           <p className={style.description}>
+//             Este sistema inteligente analiza el lavado de manos en tiempo real,
+//             detectando movimientos y asegurando un proceso adecuado.
+//           </p>
+//           <p>
+//             Modelo de servicio utilizado:{" "}
+//             <code className={style.code}>{modelName}</code>
+//           </p>
+//         </div>
+
+//         <div className={style.content}>
+//           <img
+//             src="#"
+//             ref={imageRef}
+//             onLoad={() =>
+//               detectAllClasses(
+//                 imageRef.current,
+//                 model,
+//                 canvasRef.current,
+//                 (pred) => {
+//                   console.log(pred);
+//                 }
+//               )
+//             }
+//           />
+//           <video
+//             autoPlay
+//             muted
+//             ref={cameraRef}
+//             onPlay={() =>
+//               detectVideo(
+//                 cameraRef.current,
+//                 model,
+//                 canvasRef.current,
+//                 (pred) => {
+//                   setPredicciones(pred);
+//                 }
+//               )
+//             }
+//           />
+//           <canvas
+//             width={model.inputShape[1]}
+//             height={model.inputShape[2]}
+//             ref={canvasRef}
+//           />
+//         </div>
+//         <div className={style.prediction}>
+//           <h2>Predicciones</h2>
+//           <ul>
+//             {predicciones.map((pred, index) => (
+//               <li key={index}>
+//                 {pred.clase} - {pred.score}%
+//               </li>
+//             ))}
+//           </ul>
+//         </div>
+//         <ButtonHandler
+//           imageRef={imageRef}
+//           cameraRef={cameraRef}
+//           videoRef={videoRef}
+//         />
+//       </div>
+//     </div>
+//   );
+// }
--- lavadodemanos_demo/src/utils/detect.ts
+++ lavadodemanos_demo/src/utils/detect.ts
@@ -228,3 +228,174 @@ const getMostConfidentStep = (
     confidence: maxConfidence.toFixed(2), // Format confidence to 2 decimal places
   };
 };
+
+
+//TODO: Cosas de BETO 
+
+// import * as tf from "@tensorflow/tfjs";
+// import { renderBoxes } from "./renderBox";
+// import labels from "./labels.json";
+
+// const numClass = labels.length;
+
+// /**
+//  * Preprocess image / frame before forwarded into the model
+//  * @param {HTMLVideoElement|HTMLImageElement} source
+//  * @param {Number} modelWidth
+//  * @param {Number} modelHeight
+//  * @returns input tensor, xRatio and yRatio
+//  */
+// const preprocess = (
+//   source: HTMLVideoElement | HTMLImageElement,
+//   modelWidth: number,
+//   modelHeight: number
+// ) => {
+//   let xRatio, yRatio; // ratios for boxes
+
+//   const input = tf.tidy(() => {
+//     const img = tf.browser.fromPixels(source);
+
+//     // padding image to square => [n, m] to [n, n], n > m
+//     const [h, w] = img.shape.slice(0, 2); // get source width and height
+//     const maxSize = Math.max(w, h); // get max size
+//     const imgPadded = img.pad([
+//       [0, maxSize - h], // padding y [bottom only]
+//       [0, maxSize - w], // padding x [right only]
+//       [0, 0],
+//     ]);
+
+//     xRatio = maxSize / w; // update xRatio
+//     yRatio = maxSize / h; // update yRatio
+
+//     return tf.image
+//       .resizeBilinear(imgPadded as tf.Tensor3D, [modelWidth, modelHeight]) // resize frame
+//       .div(255.0) // normalize
+//       .expandDims(0); // add batch
+//   });
+
+//   return [input, xRatio, yRatio];
+// };
+
+// /**
+//  * Function to detect video from every source.
+//  * @param {HTMLVideoElement} vidSource video source
+//  * @param {tf.GraphModel} model loaded YOLOv8 tensorflow.js model
+//  * @param {HTMLCanvasElement} canvasRef canvas reference
+//  */
+
+// export const detectAllClasses = async (
+//   source: HTMLImageElement | HTMLVideoElement,
+//   model: {
+//     net: tf.GraphModel;
+//     inputShape: number[];
+//   },
+//   canvasRef: HTMLCanvasElement,
+//   callback: (predicciones: { clase: string; score: number }[]) => void
+// ) => {
+//   if (model == null || model.inputShape == undefined) return; // handle if model is not loaded
+//   const [modelWidth, modelHeight] = model.inputShape.slice(1, 3); // get model width and height
+
+//   tf.engine().startScope(); // start scoping tf engine
+//   const [input, xRatio, yRatio] = preprocess(source, modelWidth, modelHeight); // preprocess image
+
+//   // ACA ES LA PREDICCION
+//   //EJECUTA EL MODELO SOBRE LA IMAGEN PREPROCESSADA
+//   if (!input) {
+//     throw new Error("Input tensor is undefined");
+//   }
+//   const res = model.net.execute(input) as tf.Tensor<tf.Rank>; // inference model
+//   // RES= [1, 11, 8400] 1 batch/ejemplo/imagen, 11 atributos por deteccion, 8400 detecciones
+
+//   input.dispose(); // libera de la memoria el tensor armado en preprocess, ya que no se usa mas y se pasa a ysar el res
+
+//   const transRes = res.transpose([0, 2, 1]); // transpose result [b, det, n] => [b, n, det]
+//   const rawScores = transRes.slice([0, 0, 4], [-1, -1, numClass]).squeeze(0); // #6 only squeeze axis 0 to handle only 1 class models
+//   // const scores = rawScores
+
+//   const scores = [] as number[];
+
+//   for (let i = 0; i < numClass; i++) {
+//     scores.push(
+//       rawScores.slice([0, i], [-1, 1]).flatten().max().arraySync() as number
+//     );
+//   }
+
+//   const predictionss = scores.map((score, i) => ({
+//     clase: labels[i],
+//     score: Math.ceil(score * 100),
+//   }));
+
+//   // const claseScores1 = rawScores
+//   //   .slice([0, 0], [-1, 1])
+//   //   .flatten()
+//   //   .max()
+//   //   .arraySync();
+//   // const claseScores2 = rawScores
+//   //   .slice([0, 1], [-1, 1])
+//   //   .flatten()
+//   //   .max()
+//   //   .arraySync();
+//   // const claseScores3 = rawScores
+//   //   .slice([0, 2], [-1, 1])
+//   //   .flatten()
+//   //   .max()
+//   //   .arraySync();
+//   // const claseScores4 = rawScores
+//   //   .slice([0, 3], [-1, 1])
+//   //   .flatten()
+//   //   .max()
+//   //   .arraySync();
+//   // const claseScores5 = rawScores
+//   //   .slice([0, 4], [-1, 1])
+//   //   .flatten()
+//   //   .max()
+//   //   .arraySync();
+//   // const claseScores6 = rawScores
+//   //   .slice([0, 5], [-1, 1])
+//   //   .flatten()
+//   //   .max()
+//   //   .arraySync();
+
+//   // const predictions = [
+//   //   { clase: labels[0], score: (claseScores1 * 100).toFixed(2) },
+//   //   { clase: labels[1], score: Math.ceil(claseScores2 * 100) },
+//   //   { clase: labels[2], score: (claseScores3 * 100).toFixed(2) },
+//   //   { clase: labels[3], score: (claseScores4 * 100).toFixed(2) },
+//   //   { clase: labels[4], score: (claseScores5 * 100).toFixed(2) },
+//   //   { clase: labels[5], score: (claseScores6 * 100).toFixed(2) },
+//   // ];
+
+//   callback(predictionss);
+
+//   tf.dispose([res, transRes, rawScores, input]); // cleanup memory
+
+//   tf.engine().endScope(); // end of scoping
+// };
+
+// export const detectVideo = (
+//   vidSource: HTMLVideoElement,
+//   model: {
+//     net: tf.GraphModel;
+//     inputShape: number[];
+//   },
+//   canvasRef: HTMLCanvasElement,
+//   callback: (predicciones: { claseScores: string; score: number }[]) => void
+// ) => {
+//   /**
+//    * Function to detect every frame from video
+//    */
+//   const detectFrame = async () => {
+//     if (vidSource.videoWidth === 0 && vidSource.srcObject === null) {
+//       const ctx = canvasRef.getContext("2d");
+//       ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height); // clean canvas
+//       return; // handle if source is closed
+//     }
+
+//     detectAllClasses(vidSource, model, canvasRef, (e) => {
+//       callback(e);
+//       requestAnimationFrame(detectFrame); // get another frame
+//     });
+//   };
+
+//   detectFrame(); // initialize to detect every frame
+// };
