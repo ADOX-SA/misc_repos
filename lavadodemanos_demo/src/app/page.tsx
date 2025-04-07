a9e45f0f1b2ed9abf8baf0e238d5109884a90d6a
Ivorra-1996 <Ivorra-1996>
2025-04-07 12:30:58 -0300

    correcion apagado de camara

--- lavadodemanos_demo/src/app/page.tsx
+++ lavadodemanos_demo/src/app/page.tsx
@@ -198,14 +198,14 @@ export default function Home() {
           setCurrentStep(prev => prev + 1);
           setRemainingTime(time);
           setTimerStarted(false);
-          //Valida el ultimo paso y apaga la camara.
-          webcam.close(cameraRef.current!);
-          cameraRef.current!.style.display = "none";
-          setStreaming(null);
-
-          stopDetectionRef.current?.();
-          canvasRef.current?.getContext('2d')?.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
         } else {
+            //Valida el ultimo paso y apaga la camara.
+            webcam.close(cameraRef.current!);
+            cameraRef.current!.style.display = "none";
+            setStreaming(null);
+  
+            stopDetectionRef.current?.();
+            canvasRef.current?.getContext('2d')?.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
           setInitializing(false);
         }
       } else {
